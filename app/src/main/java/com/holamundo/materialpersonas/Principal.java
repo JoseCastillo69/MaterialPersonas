c009fa52ddecad2f983c2d5eb3af975861c8208f
Roberto <roberto@morales.com.co>
2018-05-21 20:03:51 -0500

    Interfaz detalle persona en progreso

--- app/src/main/java/com/holamundo/materialpersonas/Principal.java
+++ app/src/main/java/com/holamundo/materialpersonas/Principal.java
@@ -12,6 +12,12 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 
 public class Principal extends AppCompatActivity {
@@ -20,6 +26,8 @@ public class Principal extends AppCompatActivity {
     private ArrayList<Persona> personas;
     private AdaptadorPersona adapter;
     private LinearLayoutManager llm;
+    private DatabaseReference databaseReference;
+    private String bd = "Personas";
 
 
     @Override
@@ -30,10 +38,7 @@ public class Principal extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         lstOpciones = findViewById(R.id.lstOpciones);
-        personas = new ArrayList();
-        personas.add(new Persona(R.drawable.images,"1140439156","Raul","Castro",1));
-        personas.add(new Persona(R.drawable.images2,"1140439157","Manuel","Perez",1));
-        personas.add(new Persona(R.drawable.images3,"1140439456","Sergio","Ramos",1));
+        personas = new ArrayList<>();
 
         llm = new LinearLayoutManager(this);
         llm.setOrientation(LinearLayoutManager.VERTICAL);
@@ -41,11 +46,35 @@ public class Principal extends AppCompatActivity {
 
         lstOpciones.setLayoutManager(llm);
         lstOpciones.setAdapter(adapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(bd).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                personas.clear();
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        Persona p = snapshot.getValue(Persona.class);
+                        personas.add(p);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                Datos.setPersonas(personas);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     public void crearPersonas(View v){
-        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
+        i = new Intent(Principal.this,CrearPersonas.class);
+        startActivity(i);
+
+
     }
 
 
